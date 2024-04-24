d4b0c32d70f5cc2e615b1b8d8f2b042a602f2f3b
Moy Cayanong <bpcayanong@paterostechnologicolcollege.edu.ph>
2024-04-23 16:12:43 -1200

    added course to the code

--- Prog1.java
+++ Prog1.java
@@ -1,5 +1,9 @@
   public class Prog 1 {
      public static void main(String[] args){
         System.out.println("PATEROS TECHNOLOGICAL COLLEGE");
+        System.out.println("TECHNOLOGICAL");
+        System.out.println("COLLEGE");
+        System.out.println("COURSE: BSCS");
    }
+
 }
